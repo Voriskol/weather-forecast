dab0d2c6d5115aa935db63aae062a18d4c324e2f
Kirill <pesterevkv2003@gmail.com>
2024-07-09 21:28:23 +0300

    upd: crash the system

--- src/App.vue
+++ src/App.vue
@@ -1,149 +1,182 @@
 <template>
-  <main>
-    <section class="upper-block">
-      <div class="main-info">
-        <div class="search-wrapper">
-          <input type="text" class="search" placeholder="Search for a city" />
-          <img src="@/assets/img/search.svg" alt="search" class="search__icon icon">
-        </div>
-
-        <div class="weather-info">
-          <img
-            src="@/assets/img/weather-main/thunderstorm.png"
-            alt="weather-icon"
-            class="main-info__icon icon"
-          />
-          <h2 class="temperature">14°C</h2>
-          <div class="weather-description">
-            <img class="weather-description__icon icon" src="@/assets/img/weather.svg" alt="" />
-            <span>Thunderstorm</span>
+  <div class="page">
+    <main class="main">
+      <div class="container">
+        <div class="laptop">
+          <div class="sections">
+            <section class="section section-left">
+              <div class="info">
+                <div class="city-inner">
+                  <input type="text" class="search" />
+                </div>
+                <div class="summary">
+                  <div
+                    style="background-image: url('./img/weather-main/thunderstorm.png')"
+                    class="pic-main"
+                  ></div>
+                  <div class="weather">
+                    <div class="temp">14 °C</div>
+                    <div class="weather-desc text-block">Thunderstorm</div>
+                  </div>
+                  <div class="city text-block">Paris, FR</div>
+                  <div class="date text-block">Thu, March 16, 2023</div>
+                </div>
+              </div>
+            </section>
+            <section class="section section-right">
+              <div class="section highlights">
+                <div class="title">Today's Highlights</div>
+                <div class="highlights-wrapper">
+                  <div class="highlight">
+                    <div class="card">
+                      <div class="card-title">Wind</div>
+                      <div class="card-pic card-pic--wind"></div>
+                      <div class="card-info">
+                        <div class="card-justify">
+                          <div class="info-main">
+                            <div class="info-main-num">3.6</div>
+                            <div class="info-main-text">m/s</div>
+                          </div>
+                          <div class="info-main">
+                            <div class="info-main-num">350</div>
+                            <div class="info-main-text">deg</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-small">
+                      <div class="card-small-title">Wind gusts</div>
+                      <div class="card-small-info">
+                        <div class="card-small-data">
+                          <div class="info-main-num">8.4</div>
+                          <div class="info-main-text">m/s</div>
+                        </div>
+                        <div class="card-small-hint">
+                          <div class="card-small-pic card-small-pic--wind"></div>
+                          <div class="card-small-text text-egorova">
+                            Learn
+                            <a
+                              href="https://www.windy.com/articles/weather-phenomena-what-s-the-difference-between-sustained-winds-and-wind-gusts-10390?satellite,7.787,115.115,5"
+                              target="_blank"
+                              >more</a
+                            >
+                            about gusts
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="highlight">
+                    <div class="card">
+                      <div class="card-title">Pressure</div>
+                      <div class="card-pic card-pic--pressure"></div>
+                      <div class="card-info">
+                        <div class="card-centered">
+                          <div class="info-main">
+                            <div class="info-main-num">765</div>
+                            <div class="info-main-text">mm</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-small">
+                      <div class="card-small-title">Feels like</div>
+                      <div class="card-small-info">
+                        <div class="card-small-data">
+                          <div class="info-main-num">21</div>
+                          <div class="info-main-text">°C</div>
+                        </div>
+                        <div class="card-small-hint">
+                          <div
+                            class="card-small-pic card-small-pic--margin card-small-pic--pressure"
+                          ></div>
+                          <div class="card-small-text">How hot or cold it really feels</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="highlight">
+                    <div class="card">
+                      <div class="card-title">Sunrise and sunset</div>
+                      <div class="card-pic card-pic--sun"></div>
+                      <div class="card-info">
+                        <div class="states">
+                          <div class="state">
+                            <div class="state-pic"></div>
+                            <div class="state-title">Sunrise</div>
+                            <div class="state-time">07:31:42</div>
+                          </div>
+                          <div class="state">
+                            <div class="state-pic state-pic--flipped"></div>
+                            <div class="state-title">Sunset</div>
+                            <div class="state-time">18:34:19</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="card-small">
+                      <div class="card-small-title">Cloudiness</div>
+                      <div class="card-small-info">
+                        <div class="card-small-data">
+                          <div class="info-main-num">80</div>
+                          <div class="info-main-text">%</div>
+                        </div>
+                        <div class="card-small-hint">
+                          <div class="card-small-pic card-small-pic--sun"></div>
+                          <div class="card-small-text">The sky fraction obscured by clouds</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
-        </div>
-        <div class="city-date">
-          <div class="city">
-            <img class="city__icon icon" src="@/assets/img/location.svg" alt="city icon" />
-            <p>Paris, FR</p>
-          </div>
-          <div class="date">
-            <img class="date__icon icon" src="@/assets/img/calendar.svg" alt="date icon" />
-            <p>Thu, March 16, 2023</p>
+          <div class="sections">
+            <section class="section-bottom">
+              <div class="block-bottom">
+                <div class="block-bottom-inner">
+                  <div class="block-bottom-pic pic-coords"></div>
+                  <div class="block-bottom-texts">
+                    <div class="block-bottom-text-block">
+                      <div class="block-bottom-text-block-title">Longitude: 2.3488</div>
+                      <div class="block-bottom-text-block-desc">
+                        Longitude measures distance east or west of the prime meridian.
+                      </div>
+                    </div>
+                    <div class="block-bottom-text-block">
+                      <div class="block-bottom-text-block-title">Latitude: 48.8534</div>
+                      <div class="block-bottom-text-block-desc">
+                        Latitude lines start at the equator (0 degrees latitude) and run east and
+                        west, parallel to the equator.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="section-bottom">
+              <div class="block-bottom">
+                <div class="block-bottom-inner">
+                  <div class="block-bottom-pic pic-humidity"></div>
+                  <div class="block-bottom-texts">
+                    <div class="block-bottom-text-block">
+                      <div class="block-bottom-text-block-title">Humidity: 60 %</div>
+                      <div class="block-bottom-text-block-desc">
+                        Humidity is the concentration of water vapor present in the air. Water
+                        vapor, the gaseous state of water, is generally invisible to the human eye.
+                        <br /><br />
+                        The same amount of water vapor results in higher relative humidity in cool
+                        air than warm air.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
       </div>
-      <div class="highlights">
-        <h2>Today's highlights</h2>
-        <div class="highlights-wrapper">
-          <div class="highlights-item">
-            <div class="highlights-item__main">
-              <p class="highlights-item__main-text">Wind</p>
-              <img
-                class="highlights-item__main-icon icon"
-                src="@/assets/img/equalizer (2).png"
-                alt="wind"
-              />
-              <div class="highlights-item__main-text-wrapper">
-                <p class="highlights-item__main-text">14 km/h</p>
-                <p class="highlights-item__main-text">350 deg</p>
-              </div>
-            </div>
-            <div class="highlights-item__sub">
-              <p class="highlights-item__sub-text">Wind gusts</p>
-              <div class="highlights-item__sub-text-wrapper">
-                <p class="highlights-item__sub-text">14 m/s</p>
-                <div class="highlights-item__sub-text-sub-wrapper">
-                  <img src="@/assets/img/gusts.svg" alt="gusts" class="icon highlights-item__sub-icon" />
-                  <p class="highlights-item__sub-text__gray">Learn more about gusts</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="highlights-item">
-            <div class="highlights-item__main">
-              <p class="highlights-item__main-text">Pressure</p>
-              <img
-                class="highlights-item__main-icon icon"
-                src="@/assets/img/barometer.png"
-                alt="pressure"
-              />
-              <div class="highlights-item__main-text-wrapper">
-                <p class="highlights-item__main-text">765 mm</p>
-              </div>
-            </div>
-            <div class="highlights-item__sub">
-              <p class="highlights-item__sub-text">Feels like</p>
-              <div class="highlights-item__sub-text-wrapper">
-                <p class="highlights-item__sub-text">21°C</p>
-                <div class="highlights-item__sub-text-sub-wrapper">
-                  <img src="@/assets/img/humidity.svg" alt="humidity" class="icon highlights-item__sub-icon" />
-                  <p class="highlights-item__sub-text__gray">How hot or cold it really feels</p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="highlights-item">
-            <div class="highlights-item__main">
-              <p class="highlights-item__main-text">Sunrise and sunset</p>
-              <img
-                class="highlights-item__main-icon icon"
-                src="@/assets/img/sun-moving.png"
-                alt="sun-moving"
-              />
-              <div class="highlights-item__main-text-wrapper">
-                <div class="sun sunrise">
-                  <img class="sun__icon icon" src="@/assets/img/sun.svg" alt="sunrise" />
-                  <p class="sun__text">Sunrise</p>
-                  <p class="sun__time">7:31:42</p>
-                </div>
-                <div class="sun sunset">
-                  <img class="sun__icon icon" src="@/assets/img/sun.svg" alt="sunrise" />
-                  <p class="sun__text">Sunset</p>
-                  <p class="sun__time">18:34:19</p>
-                </div>
-              </div>
-            </div>
-            <div class="highlights-item__sub">
-              <p class="highlights-item__sub-text">Cloudiness</p>
-              <div class="highlights-item__sub-text-wrapper">
-                <p class="highlights-item__sub-text">80%</p>
-                <div class="highlights-item__sub-text-sub-wrapper">
-                  <img src="@/assets/img/cloud.svg" alt="cloud" class="icon highlights-item__sub-icon" />
-                  <p class="highlights-item__sub-text__gray">The sky fraction obscured by clouds</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    <section class="bottom-block">
-      <div class="coordinates">
-        <img src="@/assets/img/compass.png" alt="compass" class="icon" />
-        <div class="coordinates-info">
-          <div class="longitude">
-            <h2 class="longitude-title">Longitude: 2.3488</h2>
-            <p class="longitude-text">Longitude measures east and west.</p>
-          </div>
-          <div class="latitude">
-            <h2 class="latitude-title">Latitude: 48.8534</h2>
-            <p class="latitude-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quia facere asperiores
-              perferendis repellendus fugit impedit quas repudiandae natus minima?
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="humidity">
-        <img src="@/assets/img/humidity.png" alt="humidity" class="icon" />
-        <div class="humidity-info">
-          <h2 class="humidity-title">Humidity: 60%</h2>
-          <p class="humidity-text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore corrupti alias
-            recusandae quo soluta mollitia ipsam laudantium, labore quod a. Commodi, reiciendis.
-            Dolorum laboriosam a voluptatum tempora nesciunt! Earum, iste.
-          </p>
-          <p class="humidity-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-      </div>
-    </section>
-  </main>
+    </main>
+  </div>
 </template>
